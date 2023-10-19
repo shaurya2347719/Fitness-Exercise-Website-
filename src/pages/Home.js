a45f0543cc45056ff9924391eb2fa7b9036eca9e
Shaurya Srivastava <shaurya2347719@users.noreply.github.com>
2023-10-19 12:26:16 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,20 +6,20 @@ import SearchExercises from "../components/SearchExercises";
 import HeroBanner from "../components/HeroBanner";
 
 const Home = () => {
-  const [bodyPart, setBodyPart] = useState("all");
+  const [bodyPart, setBodyPart] = useState("all");  //bodyPart state initialised with "all"
   const [exercises, setExercises] = useState([]);
   return (
     <Box>
       <HeroBanner />
       <SearchExercises
-        setExercises={setExercises}
-        bodyPart={bodyPart}
-        setBodyPart={setBodyPart}
+        setExercises={setExercises}  // passed {setExercises} to setExercises parameter of <SearchExercises> in SearchExercises. 
+        bodyPart={bodyPart}  // passed {bodyPart} to bodyPart parameter of <SearchExercises> in SearchExercises.js
+        setBodyPart={setBodyPart}  // passed {setBodypart} to setBodyPart parameter of <SearchExercises> in SearchExercises.js
       />
       <Exercises
-        setExercises={setExercises}
-        exercises={exercises}
-        bodyPart={bodyPart}
+        setExercises={setExercises}  // passed {setExercises} to setExercises parameter of <Exercises> in Exercises.js
+        exercises={exercises}  // passed {exercises} to exercises parameter of <Exercises> in Exercises.js
+        bodyPart={bodyPart}  // passed {bodyPart} to bodyPart parameter of <Exercises> in Exercises.js
       />
     </Box>
   );
