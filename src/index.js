42ffc819605b5df2ee44ff8138ba8fc16433e809
Shaurya Srivastava <shaurya2347719@users.noreply.github.com>
2023-10-22 16:44:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-  <BrowserRouter>
-    <App />
+root.render(  // Used for rendering a react component into root.
+  <BrowserRouter>  // Allows us to use recat router features inside the component which it wraps(Here it is App.js) like route,link and switch.
+    <App />  // App.js component is rendered.
   </BrowserRouter>
 );
